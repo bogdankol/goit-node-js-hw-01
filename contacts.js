1e96a6297b32daa955d4122c1daa7deeb6bef8c1
bogdankol <bogdank208@gmail.com>
2021-10-22 23:32:18 +0300

    all done

---
+++ contacts.js
@@ -0,0 +1,72 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+const readFile = async () => {
+  try {
+    const getheredContacts = await fs.readFile(contactsPath, "utf8");
+    return JSON.parse(getheredContacts);
+  } catch (err) {
+    console.error(`There is no such file directory!`);
+    console.error(err.message);
+  }
+};
+
+const writeToFile = async (data) => {
+  try {
+    await fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+  } catch(err) {
+    console.error(`Wrong path to file!`)
+      console.error(err.message)
+  }
+};
+
+const listContacts = async () => {
+  const data = await readFile();
+  return data;
+};
+
+const getContactById = async (contactId) => {
+  const contacts = await readFile();
+
+  const neededContact = contacts.find((el) => Number(el.id) === contactId);
+  if (neededContact) {
+    return neededContact;
+  }
+  return null;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await readFile();
+  const contactToDelete = contacts.find(el => el.id === contactId);
+  if(contactToDelete) {
+    const filteredContacts = contacts.reduce(
+        (acc, el) => {
+            Number(el.id) !== contactId ? acc.push(el) : acc
+            return acc;
+        },
+        []
+      );
+      await writeToFile(filteredContacts)
+      return filteredContacts
+  }
+  return console.log(`there is no contact with such id`)
+  
+};
+
+const addContact = async (name, email, phone) => {
+  const contacts = await readFile();
+  const id = contacts.length;
+  const newContact = { id: id + 1, name, email, phone };
+  contacts.push(newContact);
+  await writeToFile(contacts);
+  return contacts;
+};
+
+module.exports = {
+  removeContact,
+  addContact,
+  getContactById,
+  listContacts,
+};
---
+++ index.js
@@ -0,0 +1,57 @@
+const {
+  removeContact,
+  addContact,
+  getContactById,
+  listContacts,
+} = require("./contacts");
+const { Command } = require("commander");
+
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const list = await listContacts();
+      console.table(list);
+      break;
+
+    case "get":
+      const contactById = await getContactById(id);
+      if (contactById !== void 0 && contactById) {
+        console.table(contactById);
+      } else {
+        console.log(`There is no contact with that id`);
+      }
+      break;
+
+    case "add":
+      const newContactsList = await addContact(name, email, phone);
+      console.table(newContactsList);
+      break;
+
+    case "remove":
+      const newList = await removeContact(id);
+      if (newList) {
+        console.table(newList);
+      }
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+// invokeAction({action: 'list'});
+// invokeAction({action: 'get', id: 10});
+// invokeAction({action: 'add', name: 'Mango', email: "mango12@gmail.com", phone: '322-22-22'});
+invokeAction({ action: "remove", id: 5 });
